875ba0df3664651acc1e193a48c36777e5ff1e15
Aya <ayadaaboul02@outlook.com>
2025-02-04 22:25:40 +0200

    1st section in the about

---
+++ src/components/Property.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../css/Property.css'
+
+export default function Property(props){
+
+    const { img: Icon, title, description } = props;
+
+
+  return (
+    <div className='property_section'>
+        <div className="circle_property">
+        <img src={Icon} alt={title} />
+        </div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      
+    </div>
+  )
+}
+
+
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-
+import Property from '../components/Property'
 import Header from '../components/Header'
+import '../css/About.css'
+import Icon1 from "../assets/icon1.svg";
+import Icon2 from "../assets/icon2.svg";
+import Icon3 from "../assets/icon3.svg";
+
+const icons = [Icon1, Icon2, Icon3];
 
 export default function About() {
   return (
-    <div>
-     
-      <h2>THIS IS THE ABOUT PAGE</h2>
+    <>
+    <Header/>
+    <h2 className="Title_1_ab">Our Story: The Birth of Lumiere</h2>
+    
+    <div className="about_section_1">
+
+    <Property img={icons[0]} title="The Inspiration" description="Driven by passion, we created clean, effective skincare using luxurious, ethically sourced ingredients." />
+    <Property img={icons[1]} title="The Innovation" description="Lemiere became a trusted name in high-performance, eco-conscious skincare through sustainability and expert care." />
+    <Property img={icons[2]} title="Today & Beyond" description="We keep innovating with science-backed, nature-powered formulations for radiant, healthy skin." />
+
     </div>
+    
+    </>
+
+      
   )
 }
