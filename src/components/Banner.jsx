17438e1b9005180d8250ded68c60ff5d6e471d38
Aya <ayadaaboul02@outlook.com>
2025-02-05 20:26:16 +0200

    footer updates again

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import banner from '../assets/banner.jpg'
+import '../css/Banner.css'
+console.log(banner)
 export default function Banner() {
   return (
     <div className='banner_container'>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-
+import Banner from '../components/Banner'
 export default function MainPage(){
   return (
    <>
+
       <Header/>
       
       <Footer/>
